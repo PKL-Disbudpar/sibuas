85632895cc8151048acf457460935b3f9056812e
kartika18 <skartika426@gmail.com>
2024-07-19 14:13:29 +0700

    first commit

---
+++ app/Models/BukuTamu.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BukuTamu extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_tamu',
+        'nama',
+        'asal_instansi',
+        'jenis_kelamin',
+        'no_hp',
+        'tgl_pengunjung',
+        'keperluan',
+    ];
+}
