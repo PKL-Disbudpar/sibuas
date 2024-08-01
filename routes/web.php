274deeee645c5d0effb37578d6086808e1786596
Ananda Ayu <anandaayu44@gmail.com>
2024-08-01 21:42:14 +0700

    changes route

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
+Route::get('/', function () {
+    return view('home');
+});
+
 Route::get('/Login', function () {
     return view('Login');
 });
 
 Route::get('/BukuTamu', function () {
     return view('bukutamu');
-=======
-Route::get('/', function () {
-    return view('home');
->>>>>>> 1b5bf68e9a95cb8e2064e59784be6e62d1885b3e
 });
