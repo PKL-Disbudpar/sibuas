8fdd100655e4ec6fa68ddf59b57732e4c8354841
kartika18 <skartika426@gmail.com>
2024-08-14 08:50:21 +0700

    change controller buku tamu

--- app/Http/Controllers/BukuTamuController.php
+++ app/Http/Controllers/BukuTamuController.php
@@ -2,27 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\BukuTamu;
 use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Console\View\Components\Alert;
+
+use function Laravel\Prompts\alert;
 
 class BukuTamuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // menampilkan semua data
     public function index()
     {
         // $bukuTamus = BukuTamu::all();
-        // return response()->json($bukuTamus);
+        // return response()->json([
+        //     'message' => "Success Get All Data Buku Tamu",
+        //     'data' => $bukuTamus
+        // ]);
 
         return view('bukutamu');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    // menyimpan data
+    public function store(Request $request, BukuTamu $bukutamu)
     {
         $request->validate([
             'nama_tamu' => 'required|string|max:255',
@@ -33,82 +37,60 @@ class BukuTamuController extends Controller
             'keperluan' => 'required|string',
         ]);
 
-        dd($request->all());
-
         try {
-            $bukuTamu = BukuTamu::create([
-                'nama_tamu' => $request->nama_tamu,
-                'asal_instansi' => $request->asal_instansi,
-                'jenis_kelamin' => $request->jenis_kelamin,
-                'no_hp' => $request->no_hp,
-                'tgl_pengunjung' => $request->tgl_pengunjung,
-                'keperluan' => $request->keperluan,
-            ]);
+            DB::beginTransaction();
+            $bukutamu->nama_tamu = $request->nama_tamu;
+            $bukutamu->asal_instansi = $request->asal_instansi;
+            $bukutamu->jenis_kelamin = $request->jenis_kelamin;
+            $bukutamu->no_hp = $request->no_hp;
+            $bukutamu->tgl_pengunjung = $request->tgl_pengunjung;
+            $bukutamu->keperluan = $request->keperluan;
+            $bukutamu->save();
+            DB::commit();
 
-            return response()->json($bukuTamu, 201);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'Error saving data: ' . $e->getMessage()], 500);
+            if ($bukutamu->save()) {
+                echo ("Data berhasil dimasukkan");
+            } else {
+                echo "gagal";
+            }
+        } catch (Exception $e) {
+            DB::rollBack();
+            echo "Gagal nih";
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
+    // menampilkan data dari id
     public function show($id)
     {
         $bukuTamu = BukuTamu::find($id);
 
         if (is_null($bukuTamu)) {
-            return response()->json(['message' => 'Buku Tamu not found'], 404);
+            return response()->json([
+                'message' => 'Buku Tamu Not Found'
+            ], 404);
         }
 
-        return response()->json($bukuTamu);
+        return response()->json([
+            'message' => 'Success Get Data Buku Tamu by ID',
+            'data' => $bukuTamu
+        ], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id)
-    {
-        $request->validate([
-            'nama_tamu' => 'string|max:255',
-            'asal_instansi' => 'string|max:255',
-            'jenis_kelamin' => 'string|max:50',
-            'no_hp' => 'string|max:15',
-            'tgl_pengunjung' => 'date',
-            'keperluan' => 'string',
-        ]);
-
-        $bukuTamu = BukuTamu::find($id);
-
-        if (is_null($bukuTamu)) {
-            return response()->json(['message' => 'Buku Tamu not found'], 404);
-        }
-
-        $bukuTamu->update([
-            'nama_tamu' => $request->nama_tamu,
-            'asal_instansi' => $request->asal_instansi,
-            'jenis_kelamin' => $request->jenis_kelamin,
-            'no_hp' => $request->no_hp,
-            'tgl_pengunjung' => $request->tgl_pengunjung,
-            'keperluan' => $request->keperluan,
-        ]);
-
-        return response()->json($bukuTamu);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    // hapus data dari id
     public function destroy($id)
     {
         $bukuTamu = BukuTamu::find($id);
 
         if (is_null($bukuTamu)) {
-            return response()->json(['message' => 'Buku Tamu not found'], 404);
+            return response()->json([
+                'message' => 'Buku Tamu Not Found'
+            ], 404);
         }
 
         $bukuTamu->delete();
-        return response()->json(['message' => 'Buku Tamu deleted successfully']);
+
+        return response()->json([
+            'message' => 'Buku Tamu Deleted Successfully'
+        ], 200);
     }
 }
