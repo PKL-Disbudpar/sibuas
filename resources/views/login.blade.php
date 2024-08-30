8d989a18419266825d072d97aa419140400dc3fa
kartika18 <skartika426@gmail.com>
2024-08-30 11:26:23 +0700

    using blade template

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,63 @@
-<!DOCTYPE html>
+@php
+    $hideNavbar = true;
+    $hideFooter = true;
+@endphp
+
+<script>
+    function showHide() {
+        var inputan = document.getElementById("password");
+        if (inputan.type === "password") {
+            inputan.type = "text";
+        } else {
+            inputan.type = "password";
+        }
+    }
+</script>
+
+@extends('layouts.app')
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+@endpush
+
+@section('navbar')
+    
+@endsection
+
+@section('content')
+    <div class="login-container">
+        <div class="login-icon">
+            <img src="{{ asset('images/login-icon.png') }}" alt="Login Icon">
+        </div>
+        <h2>Login</h2>
+        <form action="{{ route('login.store') }}" method="POST" class="needs-validation" novalidate>
+            @csrf
+            <div class="input-group">
+                <input type="text" class="form-control" name="username" placeholder="Username" required>
+                {{-- <div class="invalid-feedback">Username tidak boleh kosong</div> --}}
+            </div>
+            <div class="input-group">
+                <input type="password" name="password" id="password" placeholder="Password" value="{{ old('password') }}" class="form-control @error('password') is-invalid @enderror"  required>
+                <i class="eye-password fa fa-regular fa-eye-slash" onclick="showHide()"></i>
+                @error('password')
+                    <span class="error invalid-feedback">{{ $message }}</span>
+                @enderror
+            </div>
+            <button type="submit" class="login-button">Masuk</button>
+        </form>
+    </div>
+@endsection
+
+@section('footer')
+    
+@endsection
+
+@push('script')
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+@endpush
+
+{{-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -64,4 +123,4 @@
     <script src="{{ asset('js/main.js') }}"></script>
 </body>
 
-</html>
+</html> --}}
