53d7cffb1cc980adf458214f19c6cf81e89df479
kartika18 <skartika426@gmail.com>
2024-08-30 13:19:06 +0700

    add access for each role

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Exception;
+use App\Models\BukuTamu;
 use App\Models\Pengguna;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -28,7 +29,24 @@ class LoginController extends Controller
                 throw new \Exception("Invalid Password");
             }
 
-            return redirect('/form-spt');
+            $data = $user->username;
+
+            // $bukuTamus = BukuTamu::all();
+
+            if ($user->id_role === 2) {
+                // Jika pengguna adalah admin, kembalikan view admin
+                return view('Super-Admin.admin-dashboard');
+            } elseif ($user->id_role === 3) {
+                // Jika pengguna adalah resepsionis, kembalikan view resepsionis
+                return view('Resepsionis.resepsionis-dashboard');
+            } elseif ($user->id_role === 4) {
+                return view('Bidang.bidang-dashboard');
+            } elseif ($data) {
+                return redirect('/form-spt')->with(compact('data'));
+            } else {
+                // Jika role tidak sesuai, redirect atau tampilkan pesan error
+                return redirect('/')->withErrors('Anda tidak memiliki akses ke halaman ini.');
+            }
         } catch (Exception $error) {
             return response()->json([
                 'error' => $error->getMessage()
